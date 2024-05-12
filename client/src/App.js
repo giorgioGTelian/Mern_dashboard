2ba92df6b97d45cb1c78a4aa3507c3f282185a8c
giorgioGTelian <giorgio.programmer@gmail.com>
2024-05-12 21:49:05 +0200

    setting up pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+import About from "./pages/about";
+import Layout from "./pages/layout";
+import Dashboard from "pages/dashboard";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode); // Get the mode
+  const theme = createTheme(themeSettings[mode]); // Get the theme
   return (
-    <div className="App">
-     
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes >
+            {/* Add all of you routes here */}
+            <Route element={<Layout />} >
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <h1>App testing</h1>
+        </ThemeProvider> 
+      </BrowserRouter>
     </div>
   );
 }
