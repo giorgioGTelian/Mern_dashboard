4ee9e790d10e5d6c735e70a5cecfaf34646cbe6f
giorgioGTelian <giorgio.programmer@gmail.com>
2024-05-18 11:58:43 +0200

    sidebar item collapse functional

--- client/src/components/sidebarTools/SidebarItemCollapse.jsx
+++ client/src/components/sidebarTools/SidebarItemCollapse.jsx
@@ -1,10 +1,10 @@
 import { Collapse, List, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import colorConfigs from "../../configs/colorConfigs";
 import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined';
 import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 import SidebarItem from "./SidebarItem";
 import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 
 const SidebarItemCollapse = ({ item }) => {
 const [open, setOpen] = useState(false);
@@ -24,14 +24,14 @@ return (
             onClick={() => setOpen(!open)}
             sx={{
                 "&: hover": {
-                backgroundColor: colorConfigs.sidebar.hoverBg
+                backgroundColor: themeSettings("light").palette.background.alt
                 },
                 paddingY: "12px",
                 paddingX: "24px"
             }}
             >
             <ListItemIcon sx={{
-                color: colorConfigs.sidebar.color
+                color: themeSettings("light").palette.background.alt
             }}>
                 {item.sidebarProps.icon && item.sidebarProps.icon}
             </ListItemIcon>
