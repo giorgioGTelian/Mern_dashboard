856ac5eb4809c293c331653991ded08a1be9faa0
giorgioGTelian <giorgio.programmer@gmail.com>
2024-05-26 19:12:52 +0200

    update theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -68,42 +68,57 @@ export const themeSettings = (mode) => {
                 ? {
                     // palette values for dark mode
                     primary: {
-                        ...tokensDark.primary,
-                        main: tokensDark.primary[400],
-                        light: tokensDark.primary[400],
+                        main: '#1976d2',
+                        light: '#2196f3',
+                        dark: '#0d47a1',
+                        contrastText: '#fff',
                     },
                     secondary: {
-                        ...tokensDark.secondary,
-                        main: tokensDark.secondary[300],
+                        light: '#FFEA41',
+                        main: '#FFE102',
+                        dark: '#DBBE01',
+                        contrastText: 'rgba(0, 0, 0, 0.87)',
                     },
-                    neutral: {
-                        ...tokensDark.grey,
-                        main: tokensDark.grey[500],
+                    text: {
+                        primary: '#EEEEEF',
+                        secondary: '#AEB0B4',
                     },
+                    divider: 'rgba(255, 255, 255, 0.12)',
                     background: {
-                        default: tokensDark.primary[600],
-                        alt: tokensDark.primary[500],
+                        paper: '#222B45',
+                        default: '#222B45',
+                        level2: '#333',
+                        level1: '#2D3748',
                     },
                 }
                 : {
                     // palette values for light mode
                     primary: {
-                        ...tokensLight.primary,
-                        main: tokensDark.grey[50],
-                        light: tokensDark.grey[100],
+                        main: '#377dff',
+                        light: '#467de3',
+                        dark: '#2f6ad9',
+                        contrastText: '#fff',
                     },
                     secondary: {
-                        ...tokensLight.secondary,
-                        main: tokensDark.secondary[600],
-                        light: tokensDark.secondary[700],
+                        light: '#ffb74d',
+                        main: '#f9b934',
+                        dark: '#FF9800',
+                        contrastText: 'rgba(0, 0, 0, 0.87)',
                     },
                     neutral: {
                         ...tokensLight.grey,
                         main: tokensDark.grey[500],
                     },
+                    text: {
+                        primary: '#1e2022',
+                        secondary: '#677788',
+                    },
+                    divider: 'rgba(0, 0, 0, 0.12)',
                     background: {
-                        default: tokensDark.grey[0],
-                        alt: tokensDark.grey[50],
+                        paper: '#ffffff',
+                        default: '#ffffff',
+                        level2: '#f5f5f5',
+                        level1: '#ffffff',
                     },
                 }),
         },
